f20800feca4d74cd77145bd53ba9288f4327d576
Micaela Raiter <micaela.raiter@pomelo.la>
2024-01-10 02:38:41 -0300

    Add phase 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChallangeController } from './challange/challange.controller';
+import { ChallangeService } from './challange/challange.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ChallangeController],
+  providers: [AppService, ChallangeService],
 })
 export class AppModule {}
---
+++ src/challange/challange.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class ChallangeService {
+  public async createCross() {
+    const margin = 2;
+    const sizeArray = 11;
+
+    try {
+      const promises = this.getPositions(sizeArray, margin).map(
+        async (position) => {
+          const data = JSON.stringify({
+            row: position[1],
+            column: position[0],
+            candidateId: process.env.CANDIDATE_ID,
+          });
+
+          const response = await axios.post(
+            `${process.env.API_URL}polyanets`,
+            data,
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            },
+          );
+          return response.data;
+        },
+      );
+
+      const results = await Promise.all(promises);
+      return results;
+    } catch (error) {
+      console.error('Error al realizar las solicitudes:', error);
+      throw error;
+    }
+  }
+
+  private isMargin(margin, sizeArray, col, row) {
+    return (
+      col < margin ||
+      row < margin ||
+      col > sizeArray - margin ||
+      row > sizeArray - margin
+    );
+  }
+
+  private getPositions(sizeArray, margin) {
+    const invertedDiagonal = [];
+    const diagonal = [];
+    for (let i = 0; i < sizeArray; i++) {
+      for (let j = 0; j < sizeArray; j++) {
+        if (
+          !this.isMargin(margin, sizeArray, i, j) &&
+          i + j === sizeArray - 1
+        ) {
+          invertedDiagonal.push([i, j]);
+        }
+        if (!this.isMargin(margin, sizeArray, i, j) && i == j) {
+          diagonal.push([i, j]);
+        }
+      }
+    }
+    return invertedDiagonal.concat(diagonal);
+  }
+}
